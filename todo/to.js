deb07145723a047bffbd15487bdcc250246dc232
Danial <daniyalghani@hotmail.com>
2015-04-27 04:43:30 -0700

    todo list through simple js

--- todo/to.js
+++ todo/to.js
@@ -1,3 +1,4 @@
+var count = 0;
 function ca(){
     var inp = document.getElementsByName("val")[0].value;
     var create = document.createElement("label"), textnode, createLabelChild;
@@ -5,20 +6,36 @@ function ca(){
     createLabelChild = document.createElement("input");
     createLabelChild.setAttribute("type","checkbox");
     createLabelChild.setAttribute("name","data");
+    createLabelChild.setAttribute("onblur","ck()");
     textnode = document.createTextNode(inp);
     create.appendChild(createLabelChild);
     create.appendChild(textnode);
     document.getElementById("m1").appendChild(create);
     document.getElementsByName("val")[0].value="";
     document.getElementsByName("val")[0].focus();
-    console.log(document.getElementsByName("data").length);
 }
 function de(){
     var i, par, grand =  document.getElementById("m1");
-    for(i = 0; i < document.getElementsByName("data").length ; i++){
-        if(document.getElementsByName("data")[i].checked){
-          par = document.getElementsByTagName("label")[i];
-            grand.removeChild(par);
+    if(count > 0) {
+        for (i = 0; i < document.getElementsByName("data").length; i++) {
+            if (document.getElementsByName("data")[i].checked) {
+                par = document.getElementsByTagName("label")[i];
+                grand.removeChild(par);
+                --i;
+            }
         }
+        count = 0;
     }
-}
+    else {
+        alert("No item found to be deleted and please select the item you want to delete");
+    }
+ }
+    function ck(){
+        count = 0;
+        for (i = 0; i < document.getElementsByName("data").length; i++) {
+            if (document.getElementsByName("data")[i].checked){
+                ++count;
+            }
+        }
+       
+    }
